a565b2d3cba8dd4e842a85da2cf25f4405ebc4fc
chadr13 <58051949+chadr13@users.noreply.github.com>
2020-08-01 13:40:59 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,8 +3,8 @@ node default {
     ensure => file,
     content => 'This is a readme',
     owner   => 'root',
+  }
 }
-
 node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
