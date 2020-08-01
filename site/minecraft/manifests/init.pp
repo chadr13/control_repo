c538a1b7302ac92a0cd36fa1e29159d957238a85
chadr13 <58051949+chadr13@users.noreply.github.com>
2020-08-01 13:49:50 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,16 +1,19 @@
 class minecraft {
-  file {‘/opt/minecraft’:
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file {‘/opt/minecraft/minecraft_server.jar’:
+  file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+    before => Service[‘minecraft’],
   }
+
+
   package {‘java’:
     ensure => present,
   }
   file {‘/opt/minecraft/eula.txt’:
-    ensure  => file,
+    ensure => file,
     content => ‘eula=true’
   }
   file {‘/etc/systemd/system/minecraft.service’:
@@ -20,5 +23,7 @@ class minecraft {
   service { ‘minecraft’:
     ensure => running,
     enable => true,
+    require => [Package[‘java’],File[‘/opt/minecraft/eula.txt’],File[‘/etc/systemd/system/minecraft.service’]],
   }
 }
+
